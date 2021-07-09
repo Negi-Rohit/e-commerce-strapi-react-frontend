9bb9096e147d5c8d1924dfdb2d813b7856e67511
Negi-Rohit <negirohit040@gmail.com>
2021-07-09 20:20:43 +0530

    products load fix

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,18 +1,26 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 import styles from "./products.module.css";
-import {API_URL, imageUrl} from "../../utils/url";
+import Spinner from "../UI/spinner/Spinner";
+import { API_URL, imageUrl } from "../../utils/url";
+import AuthContext from "../../context/AuthContext";
 
 const Products = () => {
+    const { loading, setLoading } = useContext(loading);
+
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
+        setLoading(true)
         axios
           .get(`${API_URL}/products`)
-          .then(res => setProducts(res.data))
+          .then(res => {
+              setProducts(res.data);
+              setLoading(false);
+          })
           .catch(err => console.error(err));
     },[])
     return(
@@ -27,24 +35,27 @@ const Products = () => {
                 <ResponsiveMasonry  columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}} className={styles.Products}>
                     <Masonry gutter="30px">
                     {
-                        products && (
-                            products.map(product => {
-                                return (
-                                    <div key={product.slug} className={styles.Media} >
-                                        <div className={styles.Overlay}></div>
-                                        <div>
-                                            <img src={imageUrl(product.image.formats.medium)} alt={product.slug}/>
-                                        </div>
-                                        <div className={styles.ProductDetails}>
-                                            <h1>{product.title}</h1>
-                                            <>Price: {product.price} Rs</>
-                                            <p>{product.description}</p>
-                                            <Link to={`/product/${product.slug}`}>VIEW PRODUCT</Link>
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        )
+                        loading ?
+                            <Spinner />
+                                :
+                                    products && (
+                                        products.map(product => {
+                                            return (
+                                                <div key={product.slug} className={styles.Media} >
+                                                    <div className={styles.Overlay}></div>
+                                                    <div>
+                                                        <img src={imageUrl(product.image.formats.medium)} alt={product.slug}/>
+                                                    </div>
+                                                    <div className={styles.ProductDetails}>
+                                                        <h1>{product.title}</h1>
+                                                        <>Price: {product.price} Rs</>
+                                                        <p>{product.description}</p>
+                                                        <Link to={`/product/${product.slug}`}>VIEW PRODUCT</Link>
+                                                    </div>
+                                                </div>
+                                            )
+                                        })
+                                    )
                     }
                     </Masonry>
                 </ResponsiveMasonry>
